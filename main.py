bef6b2b69c2e8396889bed16018d74b26ccfb8a1
JeeshanInamdar <jeeshaninamdar7@gmail.com>
2024-04-13 06:03:50 +0530

    Sales Completed

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+import sqlite3
 from tkinter import *
 from PIL import Image,ImageTk
 import datetime
 import time
+from tkinter import messagebox
+import os
 from employee import employeeClass
 from supplier import supplierClass
 from category import categoryClass
 from product import productClass
+from sales import saleClass
 class IMS:
     def __init__(self,root):
         self.root=root
@@ -67,7 +71,7 @@ class IMS:
         product = Button(leftFrame, text="Product",command=self.product,image=self.frame_btn_icon,compound=LEFT,padx=15,anchor="w", font=("times new roman", 20, "bold"), bg="white", bd=4,cursor="hand2").pack(side=TOP, fill=X)
 
         # Frame button Sales
-        sales = Button(leftFrame, text="Sales",image=self.frame_btn_icon,compound=LEFT,padx=15,anchor="w", font=("times new roman", 20, "bold"), bg="white", bd=4,cursor="hand2").pack(side=TOP, fill=X)
+        sales = Button(leftFrame, text="Sales",command=self.sale,image=self.frame_btn_icon,compound=LEFT,padx=15,anchor="w", font=("times new roman", 20, "bold"), bg="white", bd=4,cursor="hand2").pack(side=TOP, fill=X)
 
         # Frame button Bill
         exit = Button(leftFrame, text="Bill", image=self.frame_btn_icon, compound=LEFT, padx=15, anchor="w",font=("times new roman", 20, "bold"), bg="white", bd=4, cursor="hand2").pack(side=TOP, fill=X)
@@ -80,23 +84,29 @@ class IMS:
 
         # main Contents
         # Total Employee
-        self.lbl_employee=Label(self.root,text="Total Employee\n[ 0 ]",bg="#33bbf9",fg="white",font=("goudy old style",20,"bold"),bd=5,relief=RIDGE).place(x=375,y=120,height=150,width=300)
+        self.lbl_employee=Label(self.root,text="Total Employee\n[ 0 ]",bg="#33bbf9",fg="white",font=("goudy old style",20,"bold"),bd=5,relief=RIDGE)
+        self.lbl_employee.place(x=375,y=120,height=150,width=300)
 
         # Total Supplier
-        self.lbl_supplier = Label(self.root, text="Total Supplier\n[ 0 ]", bg="#ff5722", fg="white",font=("goudy old style", 20, "bold"), bd=5, relief=RIDGE).place(x=725, y=120,height=150, width=300)
+        self.lbl_supplier = Label(self.root, text="Total Supplier\n[ 0 ]", bg="#ff5722", fg="white",font=("goudy old style", 20, "bold"), bd=5, relief=RIDGE)
+        self.lbl_supplier.place(x=725, y=120,height=150, width=300)
 
         # Total Category
-        self.lbl_category = Label(self.root, text="Total Category\n[ 0 ]", bg="#009688", fg="white",font=("goudy old style", 20, "bold"), bd=5, relief=RIDGE).place(x=1075, y=120,height=150, width=300)
+        self.lbl_category = Label(self.root, text="Total Category\n[ 0 ]", bg="#009688", fg="white",font=("goudy old style", 20, "bold"), bd=5, relief=RIDGE)
+        self.lbl_category.place(x=1075, y=120,height=150, width=300)
 
         # Total Product
-        self.lbl_product = Label(self.root, text="Total Product\n[ 0 ]", bg="#607d8b", fg="white",font=("goudy old style", 20, "bold"), bd=5, relief=RIDGE).place(x=375, y=325,height=150, width=300)
+        self.lbl_product = Label(self.root, text="Total Product\n[ 0 ]", bg="#607d8b", fg="white",font=("goudy old style", 20, "bold"), bd=5, relief=RIDGE)
+        self.lbl_product.place(x=375, y=325,height=150, width=300)
 
         # Total Sales
-        self.lbl_sales = Label(self.root, text="Total Sales\n[ 0 ]", bg="#ffc107", fg="white",font=("goudy old style", 20, "bold"), bd=5, relief=RIDGE).place(x=725, y=325,height=150, width=300)
+        self.lbl_sales = Label(self.root, text="Total Sales\n[ 0 ]", bg="#ffc107", fg="white",font=("goudy old style", 20, "bold"), bd=5, relief=RIDGE)
+        self.lbl_sales.place(x=725, y=325,height=150, width=300)
 
         # footer
         footer=Label(self.root,text="FMS- Finance Management System | Developed by FinTech Innovators\nFor any technical issue contact : 1234567890",font=("times new roman",12),bg="#4d636d",fg="white").pack(side="bottom",fill=X)
 
+        self.update_lbl()
 #--------------------------------------------- function definations ----------------------------------
 
     def update_clock(self):
@@ -129,6 +139,41 @@ class IMS:
         self.new_win=Toplevel(self.root)
         self.new_obj=productClass(self.new_win)
 
+    def sale(self):
+        self.new_win=Toplevel(self.root)
+        self.new_obj=saleClass(self.new_win)
+
+    def update_lbl(self):
+        con=sqlite3.connect(database='ims.db')
+        cur=con.cursor()
+
+        try:
+           cur.execute("select * from employee")
+           employee=cur.fetchall()
+           self.lbl_employee.config(text=f"Total Employee\n[{str(len(employee))}]")
+
+           cur.execute("select * from category")
+           category = cur.fetchall()
+           self.lbl_category.config(text=f"Total Category\n[{str(len(category))}]")
+
+           cur.execute("select * from product")
+           product = cur.fetchall()
+           self.lbl_product.config(text=f"Total Product\n[{str(len(product))}]")
+
+           cur.execute("select * from supplier")
+           supplier = cur.fetchall()
+           self.lbl_supplier.config(text=f"Total Supplier\n[{str(len(supplier))}]")
+
+           bill=len(os.listdir('bill'))
+           self.lbl_sales.config(text=f"Total sales\n[{str(bill)}]")
+
+           #time_=time.strftime("%I:%M:%S")
+           date_=time.strftime("%d-%m-%Y")
+
+           self.clock.after(200,self.update_lbl)
+
+        except Exception as ex:
+            messagebox.showerror("Error",f"Error due to: {str(ex)}",parent=self.root)
 
 if __name__=="__main__":
     root=Tk()
